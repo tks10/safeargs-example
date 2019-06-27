88bea24b14bc17a9f7b66a1694cf7d848848ee7e
tks10 <tktktks10@gmail.com>
2019-06-27 17:21:21 +0900

    update: view

--- app/src/main/java/com/tks10/safeargsexample/FirstFragment.kt
+++ app/src/main/java/com/tks10/safeargsexample/FirstFragment.kt
@@ -1,11 +1,12 @@
 package com.tks10.safeargsexample
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_first.view.*
 
 class FirstFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -15,6 +16,8 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        view.nextButton.setOnClickListener {
+            findNavController().navigate(R.id.action_first_to_second)
+        }
     }
 }
