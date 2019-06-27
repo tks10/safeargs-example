96a0ea1e87b937fbf77696f13795596e18f55da3
tks10 <tktktks10@gmail.com>
2019-06-27 18:00:42 +0900

    feat: 3rd fragment

---
+++ app/src/main/java/com/tks10/safeargsexample/ThirdFragment.kt
@@ -0,0 +1,26 @@
+package com.tks10.safeargsexample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_first.view.*
+
+class ThirdFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.nextButton.setOnClickListener {
+            val content = view.contentEditText.text.toString()
+
+            val action = FirstFragmentDirections.actionFirstToSecond(content)
+            findNavController().navigate(action)
+        }
+    }
+}
